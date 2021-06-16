f78830261f0e2ae009eea515a331906136f4c88a
Chris Baker <chrisbaker@Chriss-MBP.hitronhub.home>
2021-06-16 13:41:12 -0400

    Test Suites, Cases, and Assertions (made basic tests)

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+// the describe() is our TEST SUITE
+describe('something truthy and falsy', () => {
+  
+  // test() is our TEST CASE
+  test('true to be true', () => {
+    // expect(true).toBe(true);
+    expect(true).toBeTruthy();
+  });
+
+  test('false to be false', () => {
+    // expect(false).toBe(false);
+    expect(false).toBeFalsy();
+  })
+})
+
+describe('App component', () => {
+  test('removes an item when clicking the Dismiss button', () => {
+
+  });
+
+  test('requests some intial stories from an API', () => {
+
+  });
+})
